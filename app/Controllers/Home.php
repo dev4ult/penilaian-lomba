86114f48349e64f6778202c2cee75b88d2341034
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-24 05:55:55 +0700

    test controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
-{
-    public function index()
-    {
-        return view('welcome_message');
+class Home extends BaseController {
+    public function index() {
+        echo view('templates/header');
+        echo view('pages/home');
+        echo view('templates/footer');
     }
 }
