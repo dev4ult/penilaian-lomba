78a6f7adb36fd30093d6b482100bc9392b822236
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-28 11:46:45 +0700

    judge controller

---
+++ app/Controllers/JudgeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class JudgeController extends BaseController {
+    public function index() {
+        // $sidebar['path'] = "/";
+
+        echo view('templates/header');
+        // echo view('templates/sidebar', $sidebar);
+        echo view('pages/manage-judge');
+        echo view('templates/footer');
+    }
+}
