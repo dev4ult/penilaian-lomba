eccc3d0e633e8a6aa6ba8b580e24432ef4b75783
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-29 06:14:05 +0700

    minor changes

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -11,57 +11,64 @@
 <main class="my-6 w-full">
 
     <!-- Management -->
-    <h2 class="text-lg font-semibold text-black/30 mb-3">Manajemen</h2>
-    <div class="flex gap-6 w-full">
-        <!-- User -->
-        <a href="/users"
-            class="no-underline w-fit bg-white border-2 hover:shadow rounded-lg py-5 px-7 cursor-pointer flex gap-4 items-center">
-            <div class="h-fit p-4 rounded-full bg-blue-300">
-                <img src="<?= base_url('./icons/user.png') ?>" class="w-8 h-8 my-0" alt="user">
-            </div>
-            <div class="">
-                <h4 class="font-medium">Akun</h4>
-                <h1 class="text-5xl mb-0 font-semibold">20</h1>
-            </div>
-        </a>
+    <div>
+        <h2 class="text-lg font-semibold text-black/30 mb-3">Manajemen</h2>
+        <div class="flex gap-6 w-full">
+            <!-- User -->
+            <a href="/users" class="no-underline w-fit bg-white border-2 hover:shadow rounded-lg py-5 px-7 cursor-pointer flex gap-4 items-center">
+                <div class="h-fit p-4 rounded-full bg-blue-300">
+                    <img src="<?= base_url('./icons/user.png') ?>" class="w-8 h-8 my-0" alt="user">
+                </div>
+                <div class="">
+                    <h4 class="font-medium">Akun</h4>
+                    <h1 class="text-5xl mb-0 font-semibold">20</h1>
+                </div>
+            </a>
 
-        <!-- Lomba  -->
-        <a href="/contests"
-            class="no-underline w-fit bg-white border-2 hover:shadow rounded-lg py-5 px-7 cursor-pointer flex gap-4 items-center">
-            <div class="h-fit p-4 rounded-full bg-purple-300">
-                <img src="<?= base_url('./icons/contest.png') ?>" class="w-8 h-8 my-0" alt="lomba">
-            </div>
-            <div class="">
-                <h4 class="font-medium">Lomba</h4>
-                <h1 class="text-5xl mb-0 font-semibold">34</h1>
-            </div>
-        </a>
+            <!-- Lomba  -->
+            <a href="/contests" class="no-underline w-fit bg-white border-2 hover:shadow rounded-lg py-5 px-7 cursor-pointer flex gap-4 items-center">
+                <div class="h-fit p-4 rounded-full bg-purple-300">
+                    <img src="<?= base_url('./icons/contest.png') ?>" class="w-8 h-8 my-0" alt="lomba">
+                </div>
+                <div class="">
+                    <h4 class="font-medium">Lomba</h4>
+                    <h1 class="text-5xl mb-0 font-semibold">34</h1>
+                </div>
+            </a>
 
-        <!-- Lomba  -->
-        <a href="/contestants"
-            class="no-underline w-fit bg-white border-2 hover:shadow rounded-lg py-5 px-7 cursor-pointer flex gap-4 items-center">
-            <div class="h-fit p-4 rounded-full bg-orange-300">
-                <img src="<?= base_url('./icons/contestant.png') ?>" class="w-8 h-8 my-0" alt="peserta">
-            </div>
-            <div class="">
-                <h4 class="font-medium">Peserta</h4>
-                <h1 class="text-5xl mb-0 font-semibold">95</h1>
-            </div>
-        </a>
+            <!-- Lomba  -->
+            <a href="/contestants" class="no-underline w-fit bg-white border-2 hover:shadow rounded-lg py-5 px-7 cursor-pointer flex gap-4 items-center">
+                <div class="h-fit p-4 rounded-full bg-orange-300">
+                    <img src="<?= base_url('./icons/contestant.png') ?>" class="w-8 h-8 my-0" alt="peserta">
+                </div>
+                <div class="">
+                    <h4 class="font-medium">Peserta</h4>
+                    <h1 class="text-5xl mb-0 font-semibold">95</h1>
+                </div>
+            </a>
 
+        </div>
     </div>
-
 
     <!-- Statistic -->
-    <h2 class="text-lg font-semibold text-black/30 mb-3 mt-10">Statistik</h2>
-    <div class="flex gap-6">
-        <div class="p-4 rounded-lg border-2 bg-white hover:shadow">
-            <div id="chart" class="min-w-[30rem]"></div>
-        </div>
-        <div class="p-4 rounded-lg border-2 bg-white hover:shadow">
-            <div id="chart-donut" class="min-w-[30rem]"></div>
+    <div class="mt-10">
+        <h2 class="text-lg font-semibold text-black/30 mb-3">Statistik</h2>
+        <div class="grid grid-flow-row grid-cols-2 gap-6">
+            <div class="p-4 rounded-lg border-2 bg-white hover:shadow">
+                <div id="chart" class="min-w-[30rem]"></div>
+            </div>
+            <div class="p-4 rounded-lg border-2 bg-white hover:shadow">
+                <div id="chart-donut" class="min-w-[30rem]"></div>
+            </div>
         </div>
     </div>
+
+    <!-- Reporting -->
+    <div class="mt-10">
+        <h2 class="text-lg font-semibold text-black/30 mb-3">Laporan</h2>
+
+    </div>
+
 </main>
 
 
