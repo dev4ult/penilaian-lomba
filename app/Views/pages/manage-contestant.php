b9486fe4ae4b8dd5e5526df80b1836bdf865793d
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-29 21:35:53 +0700

    remove modal

--- app/Views/pages/manage-contestant.php
+++ app/Views/pages/manage-contestant.php
@@ -16,73 +16,10 @@
 
 <!-- Manage Input / Button -->
 <div class="flex gap-3 justify-between mb-6 mt-8">
-    <!-- Modal Button -->
-    <button class="btn btn-neutral capitalize btn-outline self-end" onclick="registration_modal.showModal()">Registrasi
-        Peserta Baru</button>
-    <!-- Modal Form -->
-    <dialog id="registration_modal" class="modal">
-        <form method="dialog" class="modal-box p-8 max-w-2xl relative">
-            <h3 class="badge badge-lg badge-neutral mb-3">Form Registrasi</h3>
-            <p class="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, laboriosam!
-            </p>
-            <div class="grid grid-flow-row grid-cols-2 gap-6">
-                <hr class="col-span-2" />
-                <!-- Username -->
-                <div class="flex flex-col gap-1 w-full">
-                    <label for="username" class="text-sm font-semibold">Username</label>
-                    <input type="text" id="username" name="username" class="input input-bordered" placeholder="Isikan Username" required>
-                </div>
+    <!-- go to registration -->
+    <a href="/contestant/add" class="btn btn-neutral capitalize btn-outline self-end">Registrasi
+        Peserta</a>
 
-                <!-- Password -->
-                <div class="flex flex-col gap-1 w-full">
-                    <label for="password" class="text-sm font-semibold">Password</label>
-                    <input type="password" id="password" name="password" class="input input-bordered" placeholder="Isikan Password" required>
-                </div>
-
-                <!-- Role -->
-                <div class="flex flex-col gap-1 w-full">
-                    <label for="role" class="text-sm font-semibold">Role</label>
-                    <select name="role" id="role" class="select select-bordered" required>
-                        <option disabled selected>Pilih Role</option>
-                        <option value="juri">Juri</option>
-                        <option value="admin">Admin</option>
-                    </select>
-                </div>
-
-                <!-- Password Confirmation -->
-                <div class="flex flex-col gap-1 w-full">
-                    <label for="password-conf" class="text-sm font-semibold">Konfirmasi Password</label>
-                    <input type="password" id="password-conf" name="password-conf" class="input input-bordered" placeholder="Isikan Konfirmasi Password" required>
-                </div>
-
-                <hr class="col-span-2" />
-
-                <!-- NIP or NIS -->
-                <div class="flex flex-col gap-1 w-full">
-                    <label for="nip-nis" class="text-sm font-semibold">NIP / NIS</label>
-                    <input type="number" id="nip-nis" name="nip-nis" class="input input-bordered" placeholder="Isikan NIP / NIS" required>
-                </div>
-
-                <!-- Phone Number -->
-                <div class="flex flex-col gap-1 w-full">
-                    <label for="phone-number" class="text-sm font-semibold">Nomor Telepon</label>
-                    <input type="number" id="phone-number" name="phone-number" class="input input-bordered" placeholder="Isikan Nomor Telepon" required>
-                </div>
-
-                <hr class="col-span-2" />
-
-                <!-- Submit Button -->
-                <button type="submit" class="btn btn-primary capitalize col-span-2">submit</button>
-            </div>
-            <div class="modal-action my-0">
-                <button type="button" onclick="registration_modal.close()" class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </form>
-    </dialog>
 
     <!-- Search Input -->
     <div class="join">
@@ -267,7 +204,7 @@
 
         <div class="modal-action my-0">
             <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-outline btn-error capitalize">Iya</button>
-            <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-neutral capitalize">
+            <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-primary capitalize">
                 Tidak
             </button>
         </div>
