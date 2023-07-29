a3f2d50415c888c83a4b87953e35516a3a0e2584
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-30 05:49:56 +0700

    dynamic input in client side

--- public/js/contestantForm.js
+++ public/js/contestantForm.js
@@ -1,47 +1,58 @@
-$('#leader').keyup(function () {
-  $('#member-1').val(this.value);
-});
+$(document).ready(function () {
+  $('#leader').keyup(function () {
+    $('#member-1').val(this.value);
+  });
 
-$('#member-1').keyup(function () {
-  $('#leader').val(this.value);
-});
+  $('#member-1').keyup(function () {
+    $('#leader').val(this.value);
+  });
 
-function htmlVal(id) {
-  return `<hr class="col-span-2" />
-          <h2 class="badge badge-neutral">Anggota ${id}</h2>
-
-          <div class="text-right">
-              <div class="tooltip" data-tip="Jika anggota tim terdiri lebih dari satu orang">
-                  <button id="${id}" type="button" class="remove-member-btn btn btn-sm btn-error capitalize btn-outline">Hapus anggota</button>
-              </div>
-          </div>
-
-          <div class="flex flex-col gap-1 w-full">
-              <label for="member-${id}" class="text-sm font-semibold">Nama Lengkap</label>
-              <input type="text" id="member-${id}" name="member-${id}" class="input input-bordered"
-                  placeholder="Isikan Nama Lengkap" required>
-          </div>
-
-          <div class="flex flex-col gap-1 w-full">
-              <label for="nis-${id}" class="text-sm font-semibold">NIS</label>
-              <input type="number" id="nis-${id}" name="nis-${id}" class="input input-bordered"
-                  placeholder="Isikan Nomor Induk Siswa" required>
-          </div>`;
-}
-
-let totalMember = 0;
-
-$('#add-member-btn').click(function () {
-  totalMember++;
-
-  let vals = '';
-  for (let i = 0; i < totalMember; i++) {
-    vals += htmlVal(i + 2);
+  function htmlVal(id) {
+    return `<div id="member-group-${id}" class="col-span-2 grid grid-flow-row grid-cols-2 gap-6">
+                <hr class="col-span-2" />
+                <h2 class="badge badge-neutral">Anggota ${id}</h2>
+      
+                <div class="text-right">
+                    <button id="remove-${id}" type="button" class="remove-member-btn btn btn-sm btn-error capitalize btn-outline">hapus anggota</button>
+                </div>
+      
+                <div class="flex flex-col gap-1 w-full">
+                    <label for="member-${id}" class="text-sm font-semibold">Nama Lengkap</label>
+                    <input type="text" id="member-${id}" name="member-${id}" class="input input-bordered"
+                        placeholder="Isikan Nama Lengkap" required>
+                </div>
+      
+                <div class="flex flex-col gap-1 w-full">
+                    <label for="nis-${id}" class="text-sm font-semibold">NIS</label>
+                    <input type="number" id="nis-${id}" name="nis-${id}" class="input input-bordered"
+                        placeholder="Isikan Nomor Induk Siswa" required>
+                </div>
+            </div>
+    `;
   }
 
-  $('#new-member-container').html(vals);
-});
+  let totalMember = 0;
 
-$('document').click(function () {
-  console.log('test');
+  $('#add-member-btn').click(function () {
+    totalMember++;
+
+    let vals = '';
+    for (let i = 0; i < totalMember; i++) {
+      vals += htmlVal(i + 2);
+    }
+
+    $('#new-member-container').html(vals);
+  });
+
+  $(document).click(function (e) {
+    const node = e.target;
+    if (node.classList.contains('remove-member-btn')) {
+      const id = node.getAttribute('id');
+      const member_id = id.split('-')[1];
+
+      $(`#member-group-${member_id}`).remove();
+
+      totalMember--;
+    }
+  });
 });
