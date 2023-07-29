e9300c2a24704dfdc13124b19ba5137d416764c0
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-29 21:36:03 +0700

    minor changes

--- app/Views/pages/manage-user.php
+++ app/Views/pages/manage-user.php
@@ -18,7 +18,7 @@
 <div class="flex gap-3 justify-between mb-6 mt-8">
     <!-- Modal Button -->
     <button class="btn btn-neutral capitalize btn-outline self-end" onclick="registration_modal.showModal()">Registrasi
-        Akun / User Baru</button>
+        Akun / User</button>
 
     <!-- Modal Form -->
     <dialog id="registration_modal" class="modal">
@@ -31,15 +31,13 @@
                 <!-- Username -->
                 <div class="flex flex-col gap-1 w-full">
                     <label for="username" class="text-sm font-semibold">Username</label>
-                    <input type="text" id="username" name="username" class="input input-bordered"
-                        placeholder="Isikan Username" required>
+                    <input type="text" id="username" name="username" class="input input-bordered" placeholder="Isikan Username" required>
                 </div>
 
                 <!-- Password -->
                 <div class="flex flex-col gap-1 w-full">
                     <label for="password" class="text-sm font-semibold">Password</label>
-                    <input type="password" id="password" name="password" class="input input-bordered"
-                        placeholder="Isikan Password" required>
+                    <input type="password" id="password" name="password" class="input input-bordered" placeholder="Isikan Password" required>
                 </div>
 
                 <!-- Role -->
@@ -55,8 +53,7 @@
                 <!-- Password Confirmation -->
                 <div class="flex flex-col gap-1 w-full">
                     <label for="password-conf" class="text-sm font-semibold">Konfirmasi Password</label>
-                    <input type="password" id="password-conf" name="password-conf" class="input input-bordered"
-                        placeholder="Isikan Konfirmasi Password" required>
+                    <input type="password" id="password-conf" name="password-conf" class="input input-bordered" placeholder="Isikan Konfirmasi Password" required>
                 </div>
 
                 <hr class="col-span-2" />
@@ -64,15 +61,13 @@
                 <!-- NIP or NIS -->
                 <div class="flex flex-col gap-1 w-full">
                     <label for="nip-nis" class="text-sm font-semibold">NIP / NIS</label>
-                    <input type="number" id="nip-nis" name="nip-nis" class="input input-bordered"
-                        placeholder="Isikan NIP / NIS" required>
+                    <input type="number" id="nip-nis" name="nip-nis" class="input input-bordered" placeholder="Isikan NIP / NIS" required>
                 </div>
 
                 <!-- Phone Number -->
                 <div class="flex flex-col gap-1 w-full">
                     <label for="phone-number" class="text-sm font-semibold">Nomor Telepon</label>
-                    <input type="number" id="phone-number" name="phone-number" class="input input-bordered"
-                        placeholder="Isikan Nomor Telepon" required>
+                    <input type="number" id="phone-number" name="phone-number" class="input input-bordered" placeholder="Isikan Nomor Telepon" required>
                 </div>
 
                 <hr class="col-span-2" />
@@ -81,12 +76,9 @@
                 <button type="submit" class="btn btn-primary capitalize col-span-2">submit</button>
             </div>
             <div class="modal-action my-0">
-                <button type="button" onclick="registration_modal.close()"
-                    class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12" />
+                <button type="button" onclick="registration_modal.close()" class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -131,10 +123,8 @@
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
                 <td>
-                    <button class="btn btn-sm btn-warning btn-outline capitalize"
-                        onclick="edit_modal.showModal()">edit</button>
-                    <button class="btn btn-sm btn-error btn-outline capitalize"
-                        onclick="delete_modal.showModal()">hapus</button>
+                    <button class="btn btn-sm btn-warning btn-outline capitalize" onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize" onclick="delete_modal.showModal()">hapus</button>
                 </td>
             </tr>
             <tr>
@@ -143,10 +133,8 @@
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
                 <td>
-                    <button class="btn btn-sm btn-warning btn-outline capitalize"
-                        onclick="edit_modal.showModal()">edit</button>
-                    <button class="btn btn-sm btn-error btn-outline capitalize"
-                        onclick="delete_modal.showModal()">hapus</button>
+                    <button class="btn btn-sm btn-warning btn-outline capitalize" onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize" onclick="delete_modal.showModal()">hapus</button>
                 </td>
             </tr>
             <tr>
@@ -155,10 +143,8 @@
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
                 <td>
-                    <button class="btn btn-sm btn-warning btn-outline capitalize"
-                        onclick="edit_modal.showModal()">edit</button>
-                    <button class="btn btn-sm btn-error btn-outline capitalize"
-                        onclick="delete_modal.showModal()">hapus</button>
+                    <button class="btn btn-sm btn-warning btn-outline capitalize" onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize" onclick="delete_modal.showModal()">hapus</button>
                 </td>
             </tr>
             <tr>
@@ -167,10 +153,8 @@
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
                 <td>
-                    <button class="btn btn-sm btn-warning btn-outline capitalize"
-                        onclick="edit_modal.showModal()">edit</button>
-                    <button class="btn btn-sm btn-error btn-outline capitalize"
-                        onclick="delete_modal.showModal()">hapus</button>
+                    <button class="btn btn-sm btn-warning btn-outline capitalize" onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize" onclick="delete_modal.showModal()">hapus</button>
                 </td>
             </tr>
             <tr>
@@ -179,10 +163,8 @@
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
                 <td>
-                    <button class="btn btn-sm btn-warning btn-outline capitalize"
-                        onclick="edit_modal.showModal()">edit</button>
-                    <button class="btn btn-sm btn-error btn-outline capitalize"
-                        onclick="delete_modal.showModal()">hapus</button>
+                    <button class="btn btn-sm btn-warning btn-outline capitalize" onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize" onclick="delete_modal.showModal()">hapus</button>
                 </td>
             </tr>
         </tbody>
@@ -208,15 +190,13 @@
             <!-- Username -->
             <div class="flex flex-col gap-1 w-full">
                 <label for="username" class="text-sm font-semibold">Username</label>
-                <input type="text" id="username" name="username" class="input input-bordered"
-                    placeholder="Isikan Username" value="maulana02" required>
+                <input type="text" id="username" name="username" class="input input-bordered" placeholder="Isikan Username" value="maulana02" required>
             </div>
 
             <!-- Phone Number -->
             <div class="flex flex-col gap-1 w-full">
                 <label for="phone-number" class="text-sm font-semibold">Nomor Telepon</label>
-                <input type="number" id="phone-number" name="phone-number" class="input input-bordered"
-                    placeholder="Isikan Nomor Telepon" value="080000000000" required>
+                <input type="number" id="phone-number" name="phone-number" class="input input-bordered" placeholder="Isikan Nomor Telepon" value="080000000000" required>
             </div>
 
             <!-- Role -->
@@ -232,34 +212,30 @@
             <!-- NIP or NIS -->
             <div class="flex flex-col gap-1 w-full">
                 <label for="nip-nis" class="text-sm font-semibold">NIP / NIS</label>
-                <input type="number" id="nip-nis" name="nip-nis" class="input input-bordered"
-                    placeholder="Isikan NIP / NIS" value="2100000000" required>
+                <input type="number" id="nip-nis" name="nip-nis" class="input input-bordered" placeholder="Isikan NIP / NIS" value="2100000000" required>
             </div>
 
             <hr class="col-span-2" />
 
-            <!-- Password -->
-            <div class="flex flex-col gap-1 w-full">
-                <div class="flex gap-1 items-center">
-                    <label for="password" class="text-sm font-semibold">Password Baru</label>
-                    <div class="tooltip" data-tip="bersifat opsional (aman untuk
-                    dikosongkan)">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            class="stroke-current shrink-0 w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                        </svg>
-                    </div>
+            <div class="flex gap-1 items-center col-span-2">
+                <h2 class="badge badge-neutral">Ubah Password</h2>
+                <div class="tooltip" data-tip="Bersifat Opsional">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
                 </div>
-                <input type="password" id="password" name="password" class="input input-bordered"
-                    placeholder="Isikan Password">
+            </div>
+
+            <!-- New Password -->
+            <div class="flex flex-col gap-1 w-full">
+                <label for="password" class="text-sm font-semibold">Password Baru</label>
+                <input type="password" id="password" name="password" class="input input-bordered" placeholder="Isikan Password">
             </div>
 
             <!-- Password Confirmation -->
             <div class="flex flex-col gap-1 w-full self-end">
                 <label for="password-conf" class="text-sm font-semibold">Konfirmasi Password Lama</label>
-                <input type="password" id="password-conf" name="password-conf" class="input input-bordered"
-                    placeholder="Isikan Konfirmasi Password Lama">
+                <input type="password" id="password-conf" name="password-conf" class="input input-bordered" placeholder="Isikan Konfirmasi Password Lama">
             </div>
 
             <hr class="col-span-2" />
@@ -268,10 +244,8 @@
             <button type="submit" class="btn btn-warning capitalize col-span-2">simpan perubahan</button>
         </div>
         <div class="modal-action my-0">
-            <button type="button" onclick="edit_modal.close()"
-                class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
+            <button type="button" onclick="edit_modal.close()" class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </button>
@@ -287,9 +261,8 @@
         </p>
 
         <div class="modal-action my-0">
-            <button type="button" onclick="delete_modal.close()"
-                class="btn btn-sm btn-outline btn-error capitalize">Iya</button>
-            <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-neutral capitalize">
+            <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-outline btn-error capitalize">Iya</button>
+            <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-primary capitalize">
                 Tidak
             </button>
         </div>
