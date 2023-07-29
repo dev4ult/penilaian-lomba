80c5e46b35bce5c7097de3a77ffacd447132fb17
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-29 14:19:16 +0700

    registration, edit, delete form

--- app/Views/pages/manage-user.php
+++ app/Views/pages/manage-user.php
@@ -24,7 +24,8 @@
     <dialog id="registration_modal" class="modal">
         <form method="dialog" class="modal-box p-8 max-w-2xl relative">
             <h3 class="badge badge-lg badge-neutral mb-3">Form Registrasi</h3>
-            <p class="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, laboriosam!</p>
+            <p class="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, laboriosam!
+            </p>
             <div class="grid grid-flow-row grid-cols-2 gap-6">
                 <hr class="col-span-2" />
                 <!-- Username -->
@@ -81,7 +82,7 @@
             </div>
             <div class="modal-action my-0">
                 <button type="button" onclick="registration_modal.close()"
-                    class="absolute top-0 right-0 m-8 btn btn-sm btn-square btn-outline">
+                    class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -129,40 +130,55 @@
                 <td>Blue</td>
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
-                <td><button class="btn btn-sm btn-neutral btn-outline capitalize"
-                        onclick="modal_detail.showModal()">detail</button></td>
+                <td><button class="btn btn-sm btn-warning btn-outline capitalize"
+                        onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize"
+                        onclick="delete_modal.showModal()">hapus</button>
+                </td>
             </tr>
             <tr>
                 <th>2</th>
                 <td>Blue</td>
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
-                <td><button class="btn btn-sm btn-neutral btn-outline capitalize"
-                        onclick="modal_detail.showModal()">detail</button></td>
+                <td><button class="btn btn-sm btn-warning btn-outline capitalize"
+                        onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize"
+                        onclick="delete_modal.showModal()">hapus</button>
+                </td>
             </tr>
             <tr>
                 <th>3</th>
                 <td>Blue</td>
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
-                <td><button class="btn btn-sm btn-neutral btn-outline capitalize"
-                        onclick="modal_detail.showModal()">detail</button></td>
+                <td><button class="btn btn-sm btn-warning btn-outline capitalize"
+                        onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize"
+                        onclick="delete_modal.showModal()">hapus</button>
+                </td>
             </tr>
             <tr>
                 <th>4</th>
                 <td>Blue</td>
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
-                <td><button class="btn btn-sm btn-neutral btn-outline capitalize"
-                        onclick="modal_detail.showModal()">detail</button></td>
+                <td><button class="btn btn-sm btn-warning btn-outline capitalize"
+                        onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize"
+                        onclick="delete_modal.showModal()">hapus</button>
+                </td>
             </tr>
             <tr>
                 <th>5</th>
                 <td>Blue</td>
                 <td>Cy Ganderton</td>
                 <td>21XXXXXXXXXXXXXX</td>
-                <td><button class="btn btn-sm btn-neutral btn-outline capitalize"
-                        onclick="modal_detail.showModal()">detail</button></td>
+                <td><button class="btn btn-sm btn-warning btn-outline capitalize"
+                        onclick="edit_modal.showModal()">edit</button>
+                    <button class="btn btn-sm btn-error btn-outline capitalize"
+                        onclick="delete_modal.showModal()">hapus</button>
+                </td>
             </tr>
         </tbody>
     </table>
@@ -176,14 +192,101 @@
     <button class="join-item btn">4</button>
 </div>
 
-<!-- Modal Detail -->
-<dialog id="modal_detail" class="modal">
-    <form method="dialog" class="modal-box">
-        <h3 class="font-bold text-lg">Hello!</h3>
-        <p class="py-4">Press ESC key or click the button below to close</p>
-        <div class="modal-action">
-            <!-- if there is a button in form, it will close the modal -->
-            <button class="btn">Close</button>
+<!-- Modal edit -->
+<dialog id="edit_modal" class="modal">
+    <form method="dialog" class="modal-box max-w-2xl p-8 relative">
+        <h3 class="badge badge-lg badge-neutral mb-3">Edit Akun / User</h3>
+        <p class="mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, laboriosam!
+        </p>
+        <div class="grid grid-flow-row grid-cols-2 gap-6">
+            <hr class="col-span-2" />
+            <!-- Username -->
+            <div class="flex flex-col gap-1 w-full">
+                <label for="username" class="text-sm font-semibold">Username</label>
+                <input type="text" id="username" name="username" class="input input-bordered"
+                    placeholder="Isikan Username" value="maulana02" required>
+            </div>
+
+            <!-- Phone Number -->
+            <div class="flex flex-col gap-1 w-full">
+                <label for="phone-number" class="text-sm font-semibold">Nomor Telepon</label>
+                <input type="number" id="phone-number" name="phone-number" class="input input-bordered"
+                    placeholder="Isikan Nomor Telepon" value="080000000000" required>
+            </div>
+
+            <!-- Role -->
+            <div class="flex flex-col gap-1 w-full">
+                <label for="role" class="text-sm font-semibold">Role</label>
+                <select name="role" id="role" class="select select-bordered" value="juri" required>
+                    <option disabled>Pilih Role</option>
+                    <option value="juri">Juri</option>
+                    <option value="admin">Admin</option>
+                </select>
+            </div>
+
+            <!-- NIP or NIS -->
+            <div class="flex flex-col gap-1 w-full">
+                <label for="nip-nis" class="text-sm font-semibold">NIP / NIS</label>
+                <input type="number" id="nip-nis" name="nip-nis" class="input input-bordered"
+                    placeholder="Isikan NIP / NIS" value="2100000000" required>
+            </div>
+
+            <hr class="col-span-2" />
+
+            <!-- Password -->
+            <div class="flex flex-col gap-1 w-full">
+                <div class="flex gap-1 items-center">
+                    <label for="password" class="text-sm font-semibold">Password Baru</label>
+                    <div class="tooltip" data-tip="bersifat opsional (aman untuk
+                    dikosongkan)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="stroke-current shrink-0 w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <input type="password" id="password" name="password" class="input input-bordered"
+                    placeholder="Isikan Password">
+            </div>
+
+            <!-- Password Confirmation -->
+            <div class="flex flex-col gap-1 w-full self-end">
+                <label for="password-conf" class="text-sm font-semibold">Konfirmasi Password Lama</label>
+                <input type="password" id="password-conf" name="password-conf" class="input input-bordered"
+                    placeholder="Isikan Konfirmasi Password Lama">
+            </div>
+
+            <hr class="col-span-2" />
+
+            <!-- Submit Button -->
+            <button type="submit" class="btn btn-warning capitalize col-span-2">simpan perubahan</button>
+        </div>
+        <div class="modal-action my-0">
+            <button type="button" onclick="edit_modal.close()"
+                class="absolute top-0 right-0 m-8 btn btn-sm btn-circle btn-outline">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+    </form>
+</dialog>
+
+<!-- Modal Delete Confirmation -->
+<dialog id="delete_modal" class="modal">
+    <form method="dialog" class="modal-box p-8">
+        <h3 class="badge badge-lg badge-error mb-3">Hapus Akun / User</h3>
+        <p class="mb-6">Apakah anda yakin untuk menghapus Akun / User ini?
+        </p>
+
+        <div class="modal-action my-0">
+            <button type="button" onclick="delete_modal.close()"
+                class="btn btn-sm btn-outline btn-error capitalize">Iya</button>
+            <button type="button" onclick="delete_modal.close()" class="btn btn-sm btn-neutral capitalize">
+                Tidak
+            </button>
         </div>
     </form>
 </dialog>
