0985ba25a3ea7082ae6a87db2108c1b4b784322b
dev4ult <nibrasbiasaaja@gmail.com>
2023-07-30 17:21:49 +0700

    form

---
+++ app/Views/pages/add-contest.php
@@ -0,0 +1,146 @@
+<!-- Header -->
+<header class="pb-10 border-b-2 flex justify-between">
+    <div class="prose">
+        <h1 class="text-2xl font-extrabold">Tambah / Publikasi Lomba</h1>
+        <p>Pengelolaan data user untuk aksesibilitas ke aplikasi dan pengaturan peranan atau tugas terhadap user</p>
+    </div>
+
+    <!-- Breadcrumbs -->
+    <div class="text-sm breadcrumbs self-end">
+        <ul>
+            <li><a href="/" class="text-primary">Dashboard</a></li>
+            <li><a href="/contests" class="text-primary">Lomba</a></li>
+            <li><span class="badge badge-neutral rounded-full">Tambah / Publikasi</span></li>
+        </ul>
+    </div>
+</header>
+
+<form method="post" class="p-8 bg-white my-10 border-2 rounded-lg grid grid-flow-row grid-cols-2 gap-6">
+    <!-- Contest Name -->
+    <div class="flex flex-col gap-1 w-full">
+        <label for="contest-name" class="text-sm font-semibold">Nama Lomba</label>
+        <input type="text" id="contest-name" name="contest-name" class="input input-bordered"
+            placeholder="Isikan Nama Lomba" required>
+    </div>
+
+    <!-- Organizer -->
+    <div class="flex flex-col gap-1 w-full">
+        <label for="organizer" class="text-sm font-semibold">Penyelenggara</label>
+        <input type="text" id="organizer" name="organizer" class="input input-bordered"
+            placeholder="Isikan Nama Lengkap Ketua" required>
+    </div>
+
+    <!-- Description -->
+    <div class="flex flex-col gap-1 w-full col-span-2">
+        <label for="description" class="text-sm font-semibold">Deskripsi Lomba</label>
+        <textarea type="text" id="description" name="description" class="textarea textarea-bordered text-base h-52 py-3"
+            placeholder="Isikan Deskripsi Lomba" required></textarea>
+    </div>
+
+    <!-- Date -->
+    <div class="flex flex-col gap-1 w-full">
+        <label for="date" class="text-sm font-semibold">Tanggal</label>
+        <input type="date" id="date" name="date" class="input input-bordered" required>
+    </div>
+
+    <!-- Time -->
+    <div class="flex flex-col gap-1 w-full">
+        <label for="time" class="text-sm font-semibold">Waktu</label>
+        <div class="join">
+            <input type="time" id="time" name="time" class="input input-bordered join-item w-full" value="10:00"
+                required>
+            <input type="time" id="time" name="time" class="input input-bordered join-item w-full" value="11:00"
+                required>
+        </div>
+    </div>
+
+    <!-- Place -->
+    <div class="flex flex-col gap-1 w-full">
+        <label for="place" class="text-sm font-semibold">Tempat Lomba</label>
+        <input type="text" id="place" name="place" class="input input-bordered" placeholder="Isikan Nama Tempat Lomba"
+            required>
+    </div>
+
+    <hr class="col-span-2" />
+
+    <!-- Judges -->
+
+    <h2 class="badge badge-neutral self-center">Juri</h2>
+
+    <div class="flex flex-col gap-1 w-full col-span-2">
+        <div class="join w-full">
+            <select name="judge" id="judge" class="select select-bordered join-item w-full">
+                <option disabled selected>Pilih Juri</option>
+                <option value="Nibras Alyassar">Nibras Alyassar</option>
+            </select>
+            <button type="button" id="select-judge" class="btn btn-primary btn-outline capitalize">Tambah</button>
+        </div>
+    </div>
+
+    <div class="overflow-x-auto col-span-2">
+        <table class="table table-lg table-zebra border-2">
+            <thead>
+                <tr class="bg-black/20">
+                    <th></th>
+                    <th>Nama Lengkap</th>
+                    <th>Nomor Induk Pegawai / Siswa</th>
+                    <th>Nomor Telepon</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody id="judge-table">
+                <tr>
+                    <td colspan="5">
+                        <h3 class="text-center text-black/50">-- Belum ada Juri yang ditambahkan --</h3>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr class="col-span-2" />
+
+    <h2 class="badge badge-neutral self-center">Aspek Penilaian</h2>
+
+    <div class="flex flex-col gap-1 w-full col-span-2">
+        <div class="join w-full">
+            <input type="text" id="aspect" name="aspect" class="input input-bordered join-item w-full"
+                placeholder="Nama Aspek" required>
+            <select name="aspect-range" id="aspect-range" class="select select-bordered join-item">
+                <option disabled selected>Pilih Range Nilai</option>
+                <option value="1-8">1-8</option>
+                <option value="2-9">2-9</option>
+                <option value="3-10">3-10</option>
+                <option value="4-11">4-11</option>
+            </select>
+            <button type="button" id="add-aspect" class="btn btn-primary btn-outline capitalize">Tambah</button>
+        </div>
+    </div>
+
+    <div class="overflow-x-auto col-span-2">
+        <table class="table table-lg table-zebra border-2">
+            <thead>
+                <tr class="bg-black/20">
+                    <th></th>
+                    <th>Aspek</th>
+                    <th>Range Nilai</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody id="aspect-table">
+                <tr>
+                    <td colspan="4">
+                        <h3 class="text-center text-black/50">-- Belum ada Aspek yang ditambahkan --</h3>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr class="col-span-2" />
+
+    <!-- Submit Button -->
+    <button type="submit" class="btn btn-primary capitalize col-span-2">submit</button>
+</form>
+
+<script src="<?= base_url("./js/contestForm.js") ?>"></script>
