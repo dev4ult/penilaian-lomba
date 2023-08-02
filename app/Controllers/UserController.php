2730e5cba3b1e5968ffb0d2479f98f1d8855401c
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-03 06:32:41 +0700

    test findAll method

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+
 class UserController extends BaseController {
+    protected $users_model;
+    protected $data;
+
+    public function __construct() {
+        $this->users_model = new UsersModel();
+    }
+
     public function index() {
         // $sidebar['path'] = "/";
 
+
+        $this->data['users'] = $this->users_model->findAll(1);
         echo view('templates/header');
         // echo view('templates/sidebar', $sidebar);
-        echo view('pages/manage-user');
+        echo view('pages/manage-user', $this->data);
         echo view('templates/footer');
     }
 }
