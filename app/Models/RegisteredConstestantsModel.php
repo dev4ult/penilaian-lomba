23fdb5286bff92150df94cd32aae46fdd782418a
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-05 22:27:49 +0700

    new model for register a contestant

---
+++ app/Models/RegisteredConstestantsModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RegisteredConstestantsModel extends Model {
+    protected $DBGroup          = 'default';
+    protected $table            = 'registered_contestants';
+    protected $primaryKey       = 'reg_contestant_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        "reg_contestant_id",
+        "contest_id",
+        "contestant_id"
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        "contest_id" => "required",
+        "contestant_id" => "required"
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
