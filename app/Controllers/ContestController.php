e45ecc850d90701bd6ab57b109e7182ec964c751
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-05 13:50:56 +0700

    add and detail method

--- app/Controllers/ContestController.php
+++ app/Controllers/ContestController.php
@@ -3,32 +3,54 @@
 namespace App\Controllers;
 
 use App\Models\UsersModel;
+use App\Models\ContestsModel;
+use App\Models\JudgesModel;
+use App\Models\EvaluationCategoriesModel;
+
 
 class ContestController extends BaseController {
-    protected $users_model;
+    protected $users_model, $contests_model, $judges_model, $eval_categories_model;
 
     protected $data;
 
     public function __construct() {
         $this->users_model = new UsersModel();
+        $this->contests_model = new ContestsModel();
+        $this->judges_model = new JudgesModel();
+        $this->eval_categories_model = new EvaluationCategoriesModel();
     }
 
     public function index() {
         // $sidebar['path'] = "/";
 
+        $this->data['contests'] = $this->contests_model->findAll();
+
         echo view('templates/header');
         // echo view('templates/sidebar', $sidebar);
-        echo view('pages/manage-contest');
+        echo view('pages/manage-contest', $this->data);
         echo view('templates/footer');
     }
 
-    public function detail($contest_id) {
+    public function get_detail($contest_id) {
         // $sidebar['path'] = "/";
 
-        echo view('templates/header');
-        // echo view('templates/sidebar', $sidebar);
-        echo view('pages/detail-contest');
-        echo view('templates/footer');
+        $contest = $this->contests_model->find($contest_id);
+
+        if ($contest_id && $contest) {
+            $this->data['contest'] = $contest;
+            $this->data['judges'] = $this->judges_model->join('users', 'users.user_id = judges.user_id')->where('contest_id', $contest_id)->findAll();
+            $this->data['categories'] = $this->eval_categories_model->where('contest_id', $contest_id)->findAll();
+
+            echo view('templates/header');
+            // echo view('templates/sidebar', $sidebar);
+            echo view('pages/detail-contest', $this->data);
+            echo view('templates/footer');
+
+            return;
+        }
+
+        session()->setFlashdata('error', 'Lomba tidak ditemukan!');
+        return redirect()->to(base_url('contests'));
     }
 
     public function get_add() {
@@ -43,15 +65,86 @@ class ContestController extends BaseController {
     }
 
     public function post_add() {
-        $contest_data = [
+        $time_start = $this->request->getPost('time-start');
+        $time_end = $this->request->getPost('time-end');
+
+        if (strtotime((string) $time_start) >= strtotime((string) $time_end)) {
+            session()->setFlashdata('error', 'Waktu mulai tidak bisa lebih dari waktu selesai lomba');
+            return redirect()->to(base_url('contest/add'));
+        }
+
+        $post_fields = [
             'contest_name' => $this->request->getPost('contest-name'),
             'organizer' => $this->request->getPost('organizer'),
             'description' => $this->request->getPost('description'),
             'date' => $this->request->getPost('date'),
-            'time' => $this->request->getPost('time'),
+            'time-start' => $time_start,
+            'time-end' => $time_end,
             'held_on' => $this->request->getPost('held-on'),
             'category' => $this->request->getPost('category'),
         ];
+
+        $validationRules = [
+            'contest-name' => 'is_unique[contests.contest_name]'
+        ];
+
+        if (!$this->validate($validationRules)) {
+            session()->setFlashdata('error', 'Nama Lomba sudah ada atau tidak boleh sama dengan lomba lain!');
+            return redirect()->to(base_url('contest/add'));
+        }
+
+        $picture = $this->request->getFile('picture');
+
+        if ($picture->isValid()) {
+            $imageValidationRules = [
+                'picture' => 'uploaded[picture]|max_size[picture,16000]|mime_in[picture,image/jpg,image/jpeg,image/gif,image/png]'
+            ];
+
+            if (!$this->validate($imageValidationRules)) {
+                session()->setFlashdata('error', 'Gambar tidak sesuai format atau melebihi 16 mb!');
+                return redirect()->to(base_url('contest/add'));
+            }
+
+            $post_fields['picture'] = file_get_contents($picture->getTempName());
+        }
+
+        // return $this->response->setJSON($post_fields);
+
+        $insert = $this->contests_model->insert($post_fields);
+
+        if ($insert) {
+            $contest_id = $this->contests_model->getInsertID();
+
+            $total_judges = (int) $this->request->getPost('total-judges');
+            $total_eval_categories = (int) $this->request->getPost('total-eval-category');
+
+            $data_judges = [];
+            for ($i = 0; $i < $total_judges; $i++) {
+                array_push($data_judges, [
+                    "contest_id" => $contest_id,
+                    "user_id" => $this->request->getPost('judge-' . $i + 1)
+                ]);
+            }
+
+            if (count($data_judges) > 0) $this->judges_model->insertBatch($data_judges);
+
+
+
+            $data_eval_categories = [];
+            for ($i = 0; $i < $total_eval_categories; $i++) {
+                array_push($data_eval_categories, [
+                    "contest_id" => $contest_id,
+                    "category_name" => $this->request->getPost('category-' . $i + 1)
+                ]);
+            }
+
+            if (count($data_eval_categories)) $this->eval_categories_model->insertBatch($data_eval_categories);
+
+
+            session()->setFlashdata('success', 'Lomba berhasil dibuat / dipublikasikan!');
+        }
+
+        return redirect()->to(base_url('contests'));
     }
 
     public function get_edit() {
