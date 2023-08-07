4872ed4b21e30e62ffb53f3e5ea6022b6c20da9b
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-08 06:11:09 +0700

    allow multiple filter on one route

--- app/Config/Feature.php
+++ app/Config/Feature.php
@@ -7,8 +7,7 @@ use CodeIgniter\Config\BaseConfig;
 /**
  * Enable/disable backward compatibility breaking features.
  */
-class Feature extends BaseConfig
-{
+class Feature extends BaseConfig {
     /**
      * Enable multiple filters for a route or not.
      *
@@ -21,7 +20,7 @@ class Feature extends BaseConfig
      *     - property $filtersInfo, instead of $filterInfo
      *     - CodeIgniter\Router\RouteCollection::getFiltersForRoute(), instead of getFilterForRoute()
      */
-    public bool $multipleFilters = false;
+    public bool $multipleFilters = true;
 
     /**
      * Use improved new auto routing instead of the default legacy version.
