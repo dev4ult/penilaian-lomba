e9ca34ca0f8714ab6a1d608a3fddbb9d195cd51c
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-08 06:17:11 +0700

    universal changeTab

--- public/js/changeTabEval.js
+++ public/js/changeTabEval.js
@@ -1,7 +1,17 @@
+let tab = $('.tab-active').attr('id').split('-')[2];
+let indexTabActive = 0;
+
 $(document).ready(function () {
   $('.tab').click(function () {
     $('.tab').removeClass('tab-active');
 
     $(this).addClass('tab-active');
+
+    const category_id = this.id.split('-')[2];
+    const index = this.id.split('-')[3];
+
+    indexTabActive = index;
+
+    tab = category_id;
   });
 });
