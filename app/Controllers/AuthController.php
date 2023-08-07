c68b8a18a088788e38260928fb7a058ecff5c512
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-08 06:12:00 +0700

    apply login and logout method

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,10 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+
 class AuthController extends BaseController {
+    protected $users_model;
+
+    public function __construct() {
+        $this->users_model = new UsersModel();
+    }
+
     public function index() {
         echo view('templates/header');
         echo view('pages/login');
         echo view('templates/footer');
+    }
+
+    public function login() {
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+
+        $user = $this->users_model->where('username', $username)->first();
+
+        if ($user) {
+            if (password_verify((string) $password, $user['password'])) {
+                session()->set([
+                    'user_id'   => $user['user_id'],
+                    'user_role' => $user['role'],
+                    'logged_in' => true,
+                ]);
+
+                session()->setFlashdata('success', 'Berhasil login!');
+                return redirect()->to(base_url('/'));
+            }
+        }
+
+        session()->setFlashdata('error', 'Username atau password salah!');
+        return redirect()->to(base_url('login'));
+    }
+
+    public function logout() {
+        session_destroy();
+        return redirect()->to(base_url('login'));
     }
 }
