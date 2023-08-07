dc9f7f5eeb400c5a5f9d286a2d4406d3a99c3995
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-08 06:12:52 +0700

    fix action form

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center pt-24">
-    <form action="/" method="post" class="flex flex-col gap-6 bg-white p-8 border-2 rounded-lg w-[26rem] prose">
+    <form action="/login" method="post" class="flex flex-col gap-6 bg-white p-8 border-2 rounded-lg w-[26rem] prose">
         <!-- Title and  Desc-->
         <div>
             <h2 class="font-extrabold text-3xl mt-0 mb-2">Login</h2>
@@ -15,10 +15,23 @@
         <!-- Password -->
         <div class="flex flex-col gap-1 w-full">
             <label for="password" class="text-sm font-semibold">Password</label>
-            <input type="password" id="password" name="password" class="input input-bordered" placeholder="Isikan Password">
+            <input type="password" id="password" name="password" class="input input-bordered"
+                placeholder="Isikan Password">
         </div>
 
         <!-- Submit Button -->
         <button class="btn btn-primary w-full capitalize">Login</button>
     </form>
 </div>
+
+<script>
+<?php if (session()->getFlashdata('error')) : ?>
+Toastify({
+    text: `<?= session()->getFlashdata('error') ?>`,
+    close: true,
+    duration: 3000,
+    position: 'left',
+    className: 'alert alert-error fixed z-20 top-5 right-5 w-fit transition-all',
+}).showToast();
+<?php endif ?>
+</script>
