98e384df8a5c5ecbe2539417c393ef8395cba030
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-11 18:46:14 +0700

    add real statistic

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -5,10 +5,11 @@ namespace App\Controllers;
 use App\Models\UsersModel;
 use App\Models\ContestsModel;
 use App\Models\ContestantsModel;
+use App\Models\RegisteredConstestantsModel;
 
 class DashboardController extends BaseController {
 
-    protected $users_model, $contests_model, $contestants_model;
+    protected $users_model, $contests_model, $contestants_model, $reg_contestants_model;
 
     protected $data;
 
@@ -18,6 +19,7 @@ class DashboardController extends BaseController {
         $this->users_model = new UsersModel();
         $this->contests_model = new ContestsModel();
         $this->contestants_model = new ContestantsModel();
+        $this->reg_contestants_model = new RegisteredConstestantsModel();
 
         $this->user_logged_id = session('user_id');
     }
@@ -26,10 +28,13 @@ class DashboardController extends BaseController {
         // $sidebar['path'] = "/";
 
         $this->data['user'] = $this->users_model->find($this->user_logged_id);
+        $this->data['contests'] = $this->contests_model->findAll();
+        $this->data['reg_contestants'] = $this->reg_contestants_model->findAll();
 
         $this->data['total_users'] = count($this->users_model->findAll());
-        $this->data['total_contests'] = count($this->contests_model->findAll());
+        $this->data['total_contests'] = count($this->data['contests']);
         $this->data['total_contestants'] = count($this->contestants_model->findAll());
+
 
         echo view('templates/header');
         // echo view('templates/sidebar', $sidebar);
